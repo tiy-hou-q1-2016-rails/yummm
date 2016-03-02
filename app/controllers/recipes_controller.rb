f7f38a82e8de8696470bcaaa0ab14334e33b7e12
Jesse Wolgamott <jesse@comalproductions.com>
2016-03-02 11:52:49 -0600

    Update and delete

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -28,4 +28,35 @@ class RecipesController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @recipe = Recipe.find_by id: params[:id]
+  end
+
+  def update
+    # get the object
+    @recipe = Recipe.find_by id: params[:id]
+    # set the fields
+    @recipe.name = params[:recipe][:name]
+    @recipe.description = params[:recipe][:description]
+    @recipe.ingredients = params[:recipe][:ingredients]
+    @recipe.method = params[:recipe][:method]
+    @recipe.photo_url = params[:recipe][:photo_url]
+    # save redirect or render
+    if @recipe.save
+      redirect_to recipe_path(id: @recipe.id)
+    else
+      render :edit
+    end
+  end
+
+  def delete
+    # get the record
+    @recipe = Recipe.find_by id: params[:id]
+
+    # destroy the record
+    @recipe.destroy
+    # redirect away
+    redirect_to recipes_path
+  end
 end
