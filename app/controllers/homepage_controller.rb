97d14b326b30fbc87c8cc1acd76eb6c0561bb396
Jesse Wolgamott <jesse@comalproductions.com>
2016-03-09 11:49:16 -0600

    Initial Intro to authentication

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,14 @@
 class HomepageController < ApplicationController
   def show
+
+    # if we've visited before, show the number that I store
+    # else store a number, and show it to they
+
+    @number = session[:secret_number]
+    if @number.nil?
+      @number = (1..1000).to_a.sample
+      session[:secret_number] = @number
+    end
+
   end
 end
