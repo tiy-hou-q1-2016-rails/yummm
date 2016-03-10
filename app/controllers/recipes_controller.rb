577e6146baebaca18b989953854f2590d6197706
Jesse Wolgamott <jesse@comalproductions.com>
2016-03-10 10:07:21 -0600

    Switch to Strong Params

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -30,17 +30,7 @@ class RecipesController < ApplicationController
   end
 
   def create
-    # get the object
-    @recipe = Recipe.new
-    # set the fields
-    @recipe.name = params[:recipe][:name]
-    @recipe.description = params[:recipe][:description]
-    @recipe.category_id = params[:recipe][:category_id]
-    @recipe.ingredients = params[:recipe][:ingredients]
-    @recipe.method = params[:recipe][:method]
-    @recipe.photo = params[:recipe][:photo]
-    # save or redirect
-
+    @recipe = Recipe.new recipe_params
     if @recipe.save
       redirect_to recipes_path
     else
@@ -53,17 +43,8 @@ class RecipesController < ApplicationController
   end
 
   def update
-    # get the object
     @recipe = Recipe.find_by id: params[:id]
-    # set the fields
-    @recipe.name = params[:recipe][:name]
-    @recipe.category_id = params[:recipe][:category_id]
-    @recipe.description = params[:recipe][:description]
-    @recipe.ingredients = params[:recipe][:ingredients]
-    @recipe.method = params[:recipe][:method]
-    @recipe.photo = params[:recipe][:photo]
-    # save redirect or render
-    if @recipe.save
+    if @recipe.update recipe_params
       redirect_to recipe_path(id: @recipe.id)
     else
       render :edit
@@ -94,5 +75,9 @@ class RecipesController < ApplicationController
       render :show
     end
     # redirect or render
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :method, :description, :category_id, :ingredients, :photo)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
 
   validates :email, :name, :password, presence: true
+
 end
